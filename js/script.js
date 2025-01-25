ea77cb3d39f2cc4950386faff7aca20a0d88c041
Randi Febriadi <randifebriadi@gmail.com>
2025-01-25 15:55:33 +0700

    fix bug

--- js/script.js
+++ js/script.js
@@ -100,11 +100,11 @@ window.addEventListener("load", function () {
   });
 });
 
-const urlParams = new URLSearchParams(window.location.search);
-const nama = urlParams.get("n") || "";
-const pronoun = urlParams.get("p") || "Bapak/Ibu/Saudara/i";
+// const urlParams = new URLSearchParams(window.location.search);
+// const nama = urlParams.get("n") || "";
+// const pronoun = urlParams.get("p") || "Bapak/Ibu/Saudara/i";
 
-const namaContainer = document.querySelector(".hero h4 span");
-namaContainer.innerText = `${pronoun} ${nama},`.replace(/ ,$/, ",");
+// const namaContainer = document.querySelector(".hero h4 span");
+// namaContainer.innerText = `${pronoun} ${nama},`.replace(/ ,$/, ",");
 
-document.querySelector("#nama").value = nama;
+// document.querySelector("#nama").value = nama;
